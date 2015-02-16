38fea5c106c17c6b238bd39aa9b897d7acc67dbc
Mehmet Beydogan <mbbrain@gmail.com>
2015-02-16 23:14:04 +0100

    Add receiving messages from socket

--- app/scripts/controllers/channel_controller.js
+++ app/scripts/controllers/channel_controller.js
@@ -8,5 +8,20 @@ EmberChat.ChannelController = Ember.ObjectController.extend({
 			this.socket.socket.json.send(msg);
 	  		console.log('yes i send');
 	  	}
+  	},
+
+  	sockets: {
+		"message": function(data){
+			console.log(data);
+		},
+	   	// When EmberSockets makes a connection to the Socket.IO server.
+        connect: function() {
+            console.log('EmberSockets has connected...');
+        },
+
+        // When EmberSockets disconnects from the Socket.IO server.
+        disconnect: function() {
+            console.log('EmberSockets has disconnected...');
+        }
   	}
 });
